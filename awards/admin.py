cc58d5ce20ec9527f25d165d8cfb440cf53b310a
dancan <dancunsterio@gmail.com>
2020-01-11 09:11:03 -0500

    add static & media styllings

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import categories,technologies,colors,countries,Project,Profile,Rating
+
 
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    filter_horizontal =('technologies','categories','colors')
+
+
+# Register your models here.
+admin.site.register(categories)
+admin.site.register(technologies)
+admin.site.register(colors)
+admin.site.register(countries)
+admin.site.register(Project,ProjectAdmin)
+admin.site.register(Profile)
+admin.site.register(Rating)
